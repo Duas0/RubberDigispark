306ad86b4b81a4027e63757d15faa4507467d443
Duaso <isaacmduaso@gmail.com>
2023-10-20 21:46:42 +0200

    Update
    Abrir todas las pestañas que suelo usar + vscode + github

--- llegoDeClaseTweeteoLaMismaCancionYLaPongoEnYoutube/llegoDeClaseTweeteoLaMismaCancionYLaPongoEnYoutube.ino
+++ llegoDeClaseTweeteoLaMismaCancionYLaPongoEnYoutube/llegoDeClaseTweeteoLaMismaCancionYLaPongoEnYoutube.ino
@@ -1,31 +1,63 @@
 #define LAYOUT_SPANISH
 #include "DigiKeyboard.h"
+#define delay1 1000
+#define delay2 2000
 
 void setup()
 {
   DigiKeyboard.update();
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(delay2);
 
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.println("https://twitter.com/intent/tweet?url=https%3A//youtu.be/tTfn5Zir8OM%3Fsi%3D3DddLg5gFFEm8ONj&text=Black%20Hole%20-%20John%20Pollon%20%28Nightcore%29&via=YouTube&related=YouTube,YouTubeTrends,YTCreators");
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(delay2);
+
+  DigiKeyboard.println("https://twitter.com/intent/tweet?url=https%3A//youtu.be/tTfn5Zir8OM%3Fsi%3D3DddLg5gFFEm8ONj&text=Black%20Hole%20-%20John%20Pollon%20%28Nightcore%29&via=Digispark");
+  DigiKeyboard.delay(delay2);
   DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(3000);
+  DigiKeyboard.delay(delay2);
+
   DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(delay1);
   DigiKeyboard.println("https://youtu.be/tTfn5Zir8OM?si=3DddLg5gFFEm8ONj");
-  DigiKeyboard.delay(3000);
+
+  DigiKeyboard.delay(delay2);
   DigiKeyboard.sendKeyStroke(KEY_T);
-  DigiKeyboard.delay(1000);
-  for (int i = 0; i < 5; i++)
+  DigiKeyboard.delay(delay1);
+  for (int i = 0; i < 3; i++)
   {
     DigiKeyboard.sendKeyStroke(KEY_UP);
-    DigiKeyboard.delay(200);
+    DigiKeyboard.delay(delay1);
   }
 
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.println("https://campusvirtual.unex.es/portal/");
+  DigiKeyboard.delay(delay1);
+
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.println("https://mail.google.com/mail/u/1/#inbox");
+  DigiKeyboard.delay(delay1);
+
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.println("https://web.whatsapp.com/");
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(delay1);
+
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.println("github");
+  DigiKeyboard.delay(delay2);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.println("cmd");
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.println("cd \\src");
+  DigiKeyboard.delay(delay1);
+  DigiKeyboard.println("code .");
 }
 
 void loop()
